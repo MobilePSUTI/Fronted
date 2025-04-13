a53339be3d2e69c9a7a5bda5618fdb4665ca9c61
W0LIF <oleg.zapyantsev@mail.ru>
2025-04-13 14:39:30 +0400

    правки

--- Assets/script/Teacher.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-public class Teacher
-{
-    public int Id { get; set; }
-    public string Name { get; set; }
-    public string Login { get; set; }
-    // Добавьте другие поля, если необходимо
-}
